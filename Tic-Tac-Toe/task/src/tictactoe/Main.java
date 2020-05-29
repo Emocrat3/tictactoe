5a2eff32b81a497251b215508f70b1d9ea441362
Arthuro Ali Gomez <64677836+Emocrat3@users.noreply.github.com>
2020-05-29 17:36:19 -0400

    The user is the gamemaster in Tic-Tac-Toe

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\n O X O\n X X O ");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String charac = in.next();
+        System.out.println("---------");
+        System.out.println ("| " + charac.charAt (0) + " " + charac.charAt (1) + " " + charac.charAt (2) + " |");
+        System.out.println ("| " + charac.charAt (3) + " " + charac.charAt (4) + " " + charac.charAt (5) + " |");
+        System.out.println ("| " + charac.charAt (6) + " " + charac.charAt (7) + " " + charac.charAt (8) + " |");
+        System.out.println("---------");
     }
 }
